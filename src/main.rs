ed7b8e4141abc3a43cac56e6aa9d6f04516a9bd3
jtorii <59256549+jtorii@users.noreply.github.com>
2023-11-22 18:17:52 +0900

    画像ファイルのdimensionを表示するようにした (#1)
    * CHORE:Add file
    
    * FEAT:dependenciesにimageクレートを追加
    
    * FEAT:画像ファイルを開いてdimensionをプリントするようにした
    
    ---------
    
    Co-authored-by: jtorii <jun-ichi.torii@nifty.ne.jp>

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use image::GenericImageView;
+
+fn main() {
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("[USAGE] imgfile");
+        return;
+    }
+
+    let img = image::open(&args[1]).unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+}
