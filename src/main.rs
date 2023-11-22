910cc9d4904433e9eab944ec7569ce7d5b24c7c4
jtorii <jun-ichi.torii@nifty.ne.jp>
2023-11-22 18:16:09 +0900

    FEAT:画像ファイルを開いてdimensionをプリントするようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use image::GenericImageView;
+
 fn main() {
-    println!("Hello, world!");
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        println!("[USAGE] imgfile");
+        return;
+    }
+
+    let img = image::open(&args[1]).unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
 }
